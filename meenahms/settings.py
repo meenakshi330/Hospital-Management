4d81b76fbda2e4c68f4dba5ca6976c7c2f43c172
P. Meenakshi <meenakshi3032003@gmail.com>
2026-04-17 12:26:11 +0530

    fixed requirements

--- meenahms/settings.py
+++ meenahms/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_41i@z9e3xtg32si2*vej-9)%tp#c%m7b*-g4o4jhhgu@3r*4u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
